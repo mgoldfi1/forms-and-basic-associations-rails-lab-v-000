c2be32e95f190bfb9ea017fb0a0fde3a5dd37009
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-21 22:44:49 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,4 +12,13 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
 end
